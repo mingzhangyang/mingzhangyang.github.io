73b749a802fc2bdfb5fe56ea97f7382270b77d4f
yangm11 <mingzhang.yang@nih.gov>
2017-03-27 18:21:55 -0400

    update index.html

---
+++ index.js
@@ -0,0 +1,19 @@
+/**
+ * Created by yangm11 on 3/27/2017.
+ */
+'use strict';
+
+function getReady() {
+  document.getElementById('mywebapps').addEventListener('click', function () {
+    window.open('https://mingzhangyang.github.io/mywebapp/', '_blank');
+  });
+
+  document.getElementById('mybiotools').addEventListener('click', function () {
+    window.open('https://github.com/mingzhangyang/Mybiotools', '_blank');
+  });
+
+  document.getElementById('biologymeetweb').addEventListener('click', function () {
+    window.open('https://mingzhangyang.github.io/biologymeetweb/', '_blank');
+  });
+}
+
